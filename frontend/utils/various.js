07fe6bf1fa50a6b9816d6e456bf737eeaf7edd47
oriravid <oriravid@att.net>
2020-12-11 23:28:43 -0800

    Fixed sorting album by date bug

--- frontend/utils/various.js
+++ frontend/utils/various.js
@@ -12,13 +12,13 @@ export const dateSorter = (album1, album2) => {
 
     if (date1[1] > date2[1]) {
         return -1;
-    } else if (date1[0] < date2[0]) {
+    } else if (date1[1] < date2[1]) {
         return 1;
     }
 
     if (date1[2] > date2[2]) {
         return -1;
-    } else if (date1[0] < date2[0]) {
+    } else if (date1[2] < date2[2]) {
         return 1;
     }
 
